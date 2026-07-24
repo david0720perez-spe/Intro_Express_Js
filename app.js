7deda2cc43440bea7d77b0492b635fa77cb69173
David <you@example.com>
2026-07-24 14:14:34 -0500

    Trabajo con ES

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from 'express'; 
+
 const app = express();
 const port = 3000; 
 app.get("/", (_, res) => { res.send('Aprendicez ficha 3407186'); });

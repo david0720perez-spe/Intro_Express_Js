6288cd224317df772a83f96996943de36f3ea16c
David Perez <david0720perez@gmail.com>
2026-07-31 14:06:09 -0500

    dependencias dotenv, 1 forma

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express'; 
+import {configDotenv} from "dotenv"
+configDotenv
 
 const app = express();
 const port = 3000; 
 app.get("/", (_, res) => { res.send('Aprendicez ficha 3407186'); });
 
-app.listen(port, () => { console.log( `Servidor en funcionamiento en el puerto: `); });
+app.listen(port, () => { console.log( `Servidor: https://localhost:$(port) `); });

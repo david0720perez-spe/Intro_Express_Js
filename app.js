b752c59cde4dc9bdb0b6060bc504b261bdcd2ef6
David Perez <david0720perez@gmail.com>
2026-07-31 13:25:51 -0500

    Cree la rama comonjs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-import express from 'express'; 
-
+const express = require('express');
 const app = express();
 const port = 3000; 
 app.get("/", (_, res) => { res.send('Aprendicez ficha 3407186'); });
